5845363c155d3ab6e166673ce9e12cf23e771d1b
anton-andersson <112259311+anton-andersson@users.noreply.github.com>
2022-09-15 13:44:20 +0200

    Initial commit

---
+++ main.scala
@@ -0,0 +1,100 @@
+//Kvar att Göra:
+//Overtakes
+//
+import scala.runtime.stdLibPatches.language.experimental.namedTypeArguments
+@main
+def run() = {
+    //För in nuvarande ställning i ordning som strängar
+    val dStanding = Vector("Verstappen","Leclerc","Perez","Russel","Sainz","Hamilton","Norris","Ocon","Alonso","Bottas","Gasly","Magnussen","Vettel","Ricciardo","Schumacher","Tsunoda","Zhou","Stroll","Albon","Latifi")
+    val cStanding = Vector("Red Bull","Ferrair","Mercedes","Alpine","Mclaren","Alfa Romeo","Haas","Alpha Tauri","Aston Martin","Williams")
+    
+    //Lag med gp vinst respektive lag på podium
+    val cgp: Int = 2
+    // Ferrari, Mercedes, Red Bull, Mclaren, 
+    val cp: Int = 4
+    
+    //Driver-bets på respektive person
+    val dBetAnton = Vector("Verstappen","Leclerc","Hamilton","Perez","Russel","Sainz","Norris","Ricciardo","Vettel","Gasly","Alonso","Stroll","Tsunoda","Bottas","Magnussen","Ocon","Albon","Schumacher","Zhou","Latifi")
+    val dBetOscar = Vector("Hamilton","Leclerc","Verstappen","Russel","Perez","Norris","Sainz","Vettel","Ricciardo","Gasly","Bottas","Stroll","Alonso","Ocon","Tsunoda","Albon","Magnussen","Zhou","Schumacher","Latifi")
+    val dBetJoel = Vector("Verstappen","Hamilton","Russel","Perez","Sainz","Leclerc","Norris","Gasly","Ricciardo","Alonso","Vettel","Magnussen","Tsunoda","Ocon","Stroll","Schumacher","Bottas","Albon","Latifi","Zhou")
+    val dBetEmanuel = Vector("Verstappen","Sainz","Hamilton","Perez","Leclerc","Russel","Norris","Gasly","Ricciardo","Tsunoda","Vettel","Magnussen","Alonso","Bottas","Stroll","Ocon","Schumacher","Albon","Latifi","Zhou")
+    val dBetPalmer = Vector("Hamilton","Verstappen","Russel","Leclerc","Perez","Sainz","Norris","Gasly","Ricciardo","Alonso","Vettel","Tsunoda","Ocon","Magnussen","Stroll","Bottas","Schumacher","Albon","Latifi","Zhou")
+    val dBetJesper = Vector("Verstappen","Hamilton","Russel","Perez","Leclerc","Sainz","Norris","Gasly","Ricciardo","Vettel","Alonso","Ocon","Tsunoda","Stroll","Bottas","Albon","Zhou","Schumacher","Latifi","Magnussen")
+    val dBetDavid = Vector("Hamilton","Sainz","Verstappen","Russel","Leclerc","Norris","Perez","Ricciardo","Gasly","Vettel","Alonso","Ocon","Stroll","Bottas","Magnussen","Tsunoda","Albon","Schumacher","Latifi","Zhou")
+    val dBetHelmer = Vector("Hamilton","Verstappen","Leclerc","Russel","Sainz","Norris","Ricciardo","Perez","Alonso","Ocon","Vettel","Gasly","Stroll","Magnussen","Bottas","Tsunoda","Schumacher","Albon","Latifi","Zhou")
+    val dBetJohan = Vector("Hamilton","Verstappen","Sainz","Russel","Leclerc","Norris","Perez","Gasly","Ricciardo","Bottas","Alonso","Tsunoda","Ocon","Albon","Vettel","Latifi","Magnussen","Stroll","Schumacher","Zhou")
+    val dBetJonis = Vector("Verstappen","Hamilton","Russel","Perez","Leclerc","Sainz","Norris","Ricciardo","Alonso","Ocon","Vettel","Gasly","Stroll","Albon","Magnussen","Bottas","Tsunoda","Schumacher","Zhou","Latifi")    
+    val dBetPhilip = Vector("Verstappen","Hamilton","Leclerc","Sainz","Perez","Russel","Norris","Gasly","Ricciardo","Tsunoda","Alonso","Ocon","Magnussen","Vettel","Bottas","Stroll","Albon","Latifi","Schumacher","Zhou")
+    val dBetPertoft = Vector("Verstappen","Hamilton","Perez","Russel","Norris","Leclerc","Sainz","Gasly","Ricciardo","Stroll","Vettel","Ocon","Alonso","Bottas","Schumacher","Albon","Tsunoda","Magnussen","Zhou","Latifi")
+    val dBetJohanP = Vector("Verstappen","Hamilton","Leclerc","Sainz","Russel","Perez","Norris","Ricciardo","Gasly","Ocon","Vettel","Alonso","Stroll","Schumacher","Tsunoda","Albon","Magnussen","Latifi","Bottas","Zhou")
+    
+    //GP och podiumbets på respektive person
+    val cpgBetAnton: Int = 5
+    val cpBetAnton: Int = 7
+    val cpgBetOscar: Int = 4
+    val cpBetOscar: Int = 6
+    val cpgBetJoel: Int = 4
+    val cpBetJoel: Int = 7
+    val cpgBetEmanuel: Int = 4
+    val cpBetEmanuel: Int = 6
+    val cpgBetPalmer: Int = 4
+    val cpBetPalmer: Int = 7
+    val cpgBetJesper: Int = 5
+    val cpBetJesper: Int = 6
+    val cpgBetDavid: Int = 5
+    val cpBetDavid: Int = 6
+    val cpgBetHelmer: Int = 5
+    val cpBetHelmer: Int = 8
+    val cpgBetJohan: Int = 5
+    val cpBetJohan: Int = 7
+    val cpgBetJonis: Int = 4
+    val cpBetJonis: Int = 7
+    val cpgBetPhilip: Int = 5
+    val cpBetPhilip: Int = 6
+    val cpgBetPertoft: Int = 4
+    val cpBetPertoft: Int = 6
+    val cpgBetJohanP: Int = 5
+    val cpBetJohanP: Int = 7
+
+    //Poäng för respektive individ
+    var Anton: Int = 0
+    var Oscar: Int = 0
+    var Joel: Int = 0
+    var Emanuel: Int = 0
+    var Palmer: Int = 0
+    var Jesper: Int = 0
+    var David: Int = 0
+    var Helmer: Int = 0
+    var Johan: Int = 0
+    var Jonis: Int = 0
+    var Philip: Int = 0
+    var Pertoft: Int = 0
+    var JohanP: Int = 0
+
+    //Behöver tittas närmare på
+    def dScore(f: Vector[String], x: Int) = {
+        def dwinner(f: Vector[String], x: Int): Unit = {
+            if f(0) == dStanding(0) then {
+                println("Hejj")
+                
+            }
+        }
+        dwinner(f = f, x = x)
+    
+    }
+
+    def cScore() = {
+
+    }
+
+    def cgpScore() = {
+
+    }
+
+    def cpScore() = {
+
+    }
+    //kallar på dScore, första argument(f) = dbet"namn", andra argument(x) = poängvariablen ("namn")
+    dScore(f = dBetAnton, x = Anton)
+    println(Anton)
+}
